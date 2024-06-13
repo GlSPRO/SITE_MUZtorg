d7102a828bd0d39963d6f6fb63cb6c3ef6807027
GISPro <gleboss2517@yandex.ru>
2024-06-14 01:44:07 +0300

    NEW

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,20 @@ import{ Route, Routes} from 'react-router-dom';
 import Home from './components/Home';
 import Header from './components/Header';
 
+
+
 function App() {
-  const [data, setData] = useState([]);
+  
+  const [card, setCart] = useState([]);
 
   useEffect(() => {
-    const getData = async () => {
-      await axios.get('http://localhost:3001/Strings')
-      .then((res) => {
-        setData(res.data)
-        console.log(res.data)
-      })
-      .catch((error) => console.log(error))
-    }
-    getData();
-  }, [])
+    async function axiosData(){
+      const cardData = await axios.get('http://localhost:3001/Strings');
+      setCart(cardData.data);
+        }
+      axiosData();
+  }, []) 
+  
 
   return (
     <>
@@ -29,7 +29,7 @@ function App() {
       <Route
         path='/'
         element={
-          <CartItem item={data}/>
+          <CartItem item={card}/>
         }
       />
       <Route
